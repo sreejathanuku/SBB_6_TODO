187e8a7ad66ae747272b1d993bf4516dfec2ccc0
sreejathanuku <sreejathanuku563@gmail.com>
2025-03-11 15:56:41 +0530

    updated Logout

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -16,8 +16,7 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
